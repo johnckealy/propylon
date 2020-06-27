43505f1f19025cff9fe163314d30b1fdc5b5436d
John Kealy <johnckealy@gmail.com>
2020-06-27 12:08:19 +0100

    first commit

---
+++ propylon/static/propylon/js/index.js
@@ -0,0 +1,6 @@
+import '../scss/application.scss';
+import 'bootstrap';
+
+
+console.log("Hello from webpack!");
+
---
+++ propylon/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+from django.shortcuts import render
+
+
+class HomeView(TemplateView):
+    template_name = "propylon/index.html"
+
+
